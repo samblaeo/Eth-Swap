dc91f2d33418353a37e318a34a85dbc953ccb376
Edu Samblas <esmcrypto@protonmail.com>
2021-06-21 21:01:05 +0200

    Stoped at PART 2 (38:00)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,18 @@
 const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token");
 
-module.exports = function(deployer) {
-  deployer.deploy(EthSwap);
+module.exports = async function(deployer) {
+
+  // Deploy EthSwap
+  await deployer.deploy(EthSwap);
+
+  const ethSwap = await EthSwap.deployed()
+
+  //Deploy Token
+  await deployer.deploy(Token);
+
+  const token = await Token.deployed()
+
+  //Transfer all tokens to EthSwap (1 million)
+  await token.transfer(ethSwap.address, '1000000000000000000000000')
 };
